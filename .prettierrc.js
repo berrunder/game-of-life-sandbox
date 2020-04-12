6d5749b3ab9432c78ec2790c312023179c76e5f6
Alexander Cherkashin <alexander.cherkashin@csssr.com>
2020-04-12 20:01:35 +0300

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+};
